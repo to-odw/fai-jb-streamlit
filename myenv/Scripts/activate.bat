8bb4a38c790749c6b1709b0878eb0a29eb134aee
to-odw <128247001+to-odw@users.noreply.github.com>
2024-12-27 12:23:06 -0500

    Updating dir name..

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\PC\Desktop\Work\FAR AI\jb-demo\fai-jp-streamlit\myenv
+set VIRTUAL_ENV=C:\Users\PC\Desktop\Work\FAR AI\jb-demo\fai-jb-streamlit\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
